9d35c084d0d0144adb2b1ff855894d1c9aa6d984
Marshall Schmutz <schmutz.marshall@gmail.com>
2016-11-11 14:49:47 -0700

    Finish base mage, ranged, and melee classes

--- CCTowers/Unit.swift
+++ CCTowers/Unit.swift
@@ -15,6 +15,7 @@ import Foundation
 //    var unitRange: Int { get set }
 //}
 
+// Melee class
 class Melee {
     var unitHealth: Int
     var unitAttack: Int
@@ -74,6 +75,162 @@ class Melee {
     }
     
 //  Other methods
+    func upgrade() {
+        // When upgraded, boost stats
+        self.unitHealth = getHealth() + 10
+        self.unitAttack = getAttack() + 10
+        self.unitDefense = getDefense() + 10
+        self.unitRange = getRange() + 10
+        self.unitSpeed = getSpeed() + 10
+    }
+    
+    func isDead() -> Bool {
+        if isLive {
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+// Ranged class
+class Ranged {
+    var unitHealth: Int
+    var unitAttack: Int
+    var unitDefense: Int
+    var unitRange: Int
+    var unitSpeed: Int
+    var isLive: Bool = true
+    
+    init(unitHealth: Int, unitAttack: Int, unitDefense: Int, unitRange: Int, unitSpeed: Int) {
+        self.unitHealth = unitHealth
+        self.unitAttack = unitAttack
+        self.unitDefense = unitDefense
+        self.unitRange = unitDefense
+        self.unitSpeed = unitSpeed
+    }
+    
+    //  All the getters
+    func getHealth() -> Int {
+        return self.unitHealth
+    }
+    
+    func getAttack() -> Int {
+        return self.unitAttack
+    }
+    
+    func getDefense() -> Int {
+        return self.unitDefense
+    }
+    
+    func getRange() -> Int {
+        return self.unitRange
+    }
+    
+    func getSpeed() -> Int {
+        return self.unitSpeed
+    }
+    
+    //  All the setters
+    func setHealth(health: Int) {
+        self.unitHealth = health
+    }
+    
+    func setAttach(attack: Int) {
+        self.unitAttack = attack
+    }
+    
+    func setDefense(defense: Int) {
+        self.unitDefense = defense
+    }
+    
+    func setRange(range: Int) {
+        self.unitRange = range
+    }
+    
+    func setSpeed(speed: Int) {
+        self.unitSpeed = speed
+    }
+    
+    //  Other methods
+    func upgrade() {
+        // When upgraded, boost stats
+        self.unitHealth = getHealth() + 10
+        self.unitAttack = getAttack() + 10
+        self.unitDefense = getDefense() + 10
+        self.unitRange = getRange() + 10
+        self.unitSpeed = getSpeed() + 10
+    }
+    
+    func isDead() -> Bool {
+        if isLive {
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+// Mage class
+class Mage {
+    var unitHealth: Int
+    var unitAttack: Int
+    var unitDefense: Int
+    var unitRange: Int
+    var unitSpeed: Int
+    var isLive: Bool = true
+    
+    init(unitHealth: Int, unitAttack: Int, unitDefense: Int, unitRange: Int, unitSpeed: Int) {
+        self.unitHealth = unitHealth
+        self.unitAttack = unitAttack
+        self.unitDefense = unitDefense
+        self.unitRange = unitDefense
+        self.unitSpeed = unitSpeed
+    }
+    
+    //  All the getters
+    func getHealth() -> Int {
+        return self.unitHealth
+    }
+    
+    func getAttack() -> Int {
+        return self.unitAttack
+    }
+    
+    func getDefense() -> Int {
+        return self.unitDefense
+    }
+    
+    func getRange() -> Int {
+        return self.unitRange
+    }
+    
+    func getSpeed() -> Int {
+        return self.unitSpeed
+    }
+    
+    //  All the setters
+    func setHealth(health: Int) {
+        self.unitHealth = health
+    }
+    
+    func setAttach(attack: Int) {
+        self.unitAttack = attack
+    }
+    
+    func setDefense(defense: Int) {
+        self.unitDefense = defense
+    }
+    
+    func setRange(range: Int) {
+        self.unitRange = range
+    }
+    
+    func setSpeed(speed: Int) {
+        self.unitSpeed = speed
+    }
+    
+    //  Other methods
     func upgrade() {
         // When upgraded, boost stats
         self.unitHealth = getHealth() + 10
