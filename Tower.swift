1f0cc509eebaa00b7a10e6a322332b7b942a6fc1
Marshall Schmutz <schmutz.marshall@gmail.com>
2016-11-11 13:37:48 -0700

    A tower file

---
+++ Tower.swift
@@ -0,0 +1,37 @@
+//
+//  Tower.swift
+//  CCTowers
+//
+//  Created by Marshall Schmutz on 11/11/16.
+//  Copyright © 2016 DxATC. All rights reserved.
+//
+
+import Foundation
+
+protocol Base {
+    var towerHealth: Int { get set }
+//    func getHealth()
+//    func setHealth()
+}
+
+class Tower: Base {
+    var towerHealth: Int
+    var gold: Int
+    
+    init(towerHealth: Int) {
+        self.towerHealth = towerHealth
+        self.gold = 0
+    }
+    
+    func getHealth() -> Int {
+        return self.towerHealth
+    }
+    
+    func setHealth(health: Int) {
+        self.towerHealth = health
+    }
+    
+    func goldGen(kills: Int) {
+        self.gold += kills
+    }
+}
