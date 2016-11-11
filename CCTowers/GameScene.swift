e89fb89bf301a6354bccfe3f5cd2922e5479b27a
Marshall Schmutz <schmutz.marshall@gmail.com>
2016-11-11 10:14:03 -0700

    My tinkering

--- CCTowers/GameScene.swift
+++ CCTowers/GameScene.swift
@@ -20,7 +20,7 @@ class GameScene: SKScene {
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+            label.run(SKAction.fadeIn(withDuration: 3.0))
         }
         
         // Create shape node to use during mouse interaction
@@ -28,9 +28,9 @@ class GameScene: SKScene {
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
         if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+            spinnyNode.lineWidth = 1.5
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 0.5)))
             spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
@@ -57,7 +57,7 @@ class GameScene: SKScene {
     func touchUp(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
-            n.strokeColor = SKColor.red
+            n.strokeColor = SKColor.yellow
             self.addChild(n)
         }
     }
---
+++ CCTowers/TestFile.swift
@@ -0,0 +1,9 @@
+//
+//  TestFile.swift
+//  CCTowers
+//
+//  Created by Marshall Schmutz on 11/11/16.
+//  Copyright © 2016 DxATC. All rights reserved.
+//
+
+import Foundation
