b00fa446856c782fe08a9d9499cdb60aac786d48
SoulxDragon <Soulxdragon@gmail.com>
2016-11-11 11:25:48 -0700

    Overriding Marshall

--- CCTowers/GameScene.swift
+++ CCTowers/GameScene.swift
@@ -49,7 +49,7 @@ class GameScene: SKScene {
     func touchMoved(toPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
-            n.strokeColor = SKColor.blue
+            n.strokeColor = SKColor.magenta
             self.addChild(n)
         }
     }
