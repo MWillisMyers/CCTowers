ca56fcca3a5d9110efa7e2209e57044be28abbc2
Marshall Schmutz <schmutz.marshall@gmail.com>
2016-11-11 19:57:25 -0700

    Work on xy coords for unit and projectiles

--- CCTowers/Projectile.swift
+++ CCTowers/Projectile.swift
@@ -11,9 +11,13 @@ import Foundation
 class Projectile {
     var projSpeed: Int
     var isAlive: Bool = true
+    var posX: Int
+    var posY: Int
     
-    init(projSpeed: Int) {
+    init(projSpeed: Int, posX: Int, posY: Int) {
         self.projSpeed = projSpeed
+        self.posX = posX
+        self.posY = posY
     }
     
     func isDead() -> Bool {
