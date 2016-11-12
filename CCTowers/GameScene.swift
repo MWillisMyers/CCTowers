052c44832fdb4144f829f804954120d8e59e69c5
Marshall Schmutz <schmutz.marshall@gmail.com>
2016-11-11 23:03:53 -0700

    my feeble attempt at animation

--- CCTowers/GameScene.swift
+++ CCTowers/GameScene.swift
@@ -13,91 +13,27 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    let player = SKSpriteNode(imageNamed: "Spaceship")
-    
+    var rangerNode = SKSpriteNode()
+    var rangerAtlas = SKTextureAtlas()
+    var rangerArray = [SKTexture]()
 
     override func didMove(to view: SKView) {
-        super.didMove(to: view)
-        // 1
-        let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
-        // 2
-        borderBody.friction = 0
-        // 3
-        self.physicsBody = borderBody
-
-    
-    
-
-        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        rangerAtlas = SKTextureAtlas(named: "ArcherWalking")
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 3.0))
+        for var i in 0...rangerAtlas.textureNames.count {
+            var Name = "Archer Girl_\(i).png"
+            rangerArray.append(SKTexture(imageNamed: Name))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 1.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 0.5)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-        
+        rangerNode = SKSpriteNode(imageNamed: rangerAtlas.textureNames[0])
+        rangerNode.size = CGSize(width: 70, height: 140)
+        rangerNode.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        self.addChild(rangerNode)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.cyan
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.yellow
-            self.addChild(n)
-        }
-    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
