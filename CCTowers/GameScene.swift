414f9803e0c92e6d9a870a72660714f3911dbec7
SoulxDragon <Soulxdragon@gmail.com>
2016-11-12 00:47:05 -0700

    Fill fixes FO SHO MILADY

--- CCTowers/GameScene.swift
+++ CCTowers/GameScene.swift
@@ -13,22 +13,10 @@ import SpriteKit
 
 class GameScene: SKScene {
     
-    var rangerNode = SKSpriteNode()
-    var rangerAtlas = SKTextureAtlas()
-    var rangerArray = [SKTexture]()
+    
 
     override func didMove(to view: SKView) {
-        rangerAtlas = SKTextureAtlas(named: "ArcherWalking")
         
-        //for var i in 0...rangerAtlas.textureNames.count {
-    //        var Name = "Archer Girl_\(i).png"
-      //      rangerArray.append(SKTexture(imageNamed: Name))
-    //}
-    
-        //rangerNode = SKSpriteNode(imageNamed: rangerAtlas.textureNames[0])
-      //  rangerNode.size = CGSize(width: 70, height: 140)
-        //rangerNode.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-       // self.addChild(rangerNode)
     }
     
     
